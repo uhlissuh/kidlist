ccc6263c4f5cbff910f0bf5db503b1d355924610
Alissa Sobo <alissasobo@gmail.com>
2015-12-21 15:25:37 -0800

    add express handlebars

--- server.js
+++ server.js
@@ -2,17 +2,18 @@ var http = require("http");
 var fs = require("fs");
 var express = require("express");
 var bodyParser = require("body-parser");
+var handlebars = require("express-handlebars");
 
 var app = express();
 
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-  fs.readFile("index.html", "utf8", function(err, data) {
-    res.end(data);
-  });
-})
+  res.render('home');
+});
 
 var server = http.createServer(app);
 server.listen(8080);

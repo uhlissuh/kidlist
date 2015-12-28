b4a9771203e7626eab66afc7460ec286f757f721
Alissa Sobo <alissasobo@gmail.com>
2015-12-27 22:34:19 -0800

    add password encryption

--- database.js
+++ database.js
@@ -1,6 +1,8 @@
 var pg = require('pg');
 var conString = "postgres://alissa:@localhost/kidlist";
+var bcrypt = require("bcrypt");
 
+var SALT = '$2a$10$pmAjS4LJeLKXhKC9GutBl.';
 
 exports.createUser = function(data, callback) {
   pg.connect(conString, function(err, client, done) {
@@ -9,14 +11,15 @@ exports.createUser = function(data, callback) {
       return;
     }
 
-
-    client.query(
-      "insert into users (first_name, last_name, name_business, city, state, email, password_hash) values ($1, $2, $3, $4, $5, $6, $7)",
-      [data.first_name, data.last_name, data.biz_name, data.city, data.state, data.email, data.password],
-      function(err, result) {
-        done();
-        callback(err);
-      }
-    );
+    bcrypt.hash(data.password, SALT, function(err, hash) {
+      client.query(
+        "insert into users (first_name, last_name, name_business, city, state, email, password_hash) values ($1, $2, $3, $4, $5, $6, $7)",
+        [data.first_name, data.last_name, data.biz_name, data.city, data.state, data.email, hash],
+        function(err, result) {
+          done();
+          callback(err);
+        }
+      );
+    });
   });
 }
